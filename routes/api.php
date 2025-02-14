f0e8d390a608b3439873fd50eb83ff9513329a69
incrisz <incrisz4luv@gmail.com>
2025-02-14 19:22:42 +0100

    f

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,10 @@ use Illuminate\Support\Facades\Route;
 
 
 use App\Http\Controllers\V1\SubjectController;
+use Illuminate\Http\Request;
 
-Route::apiResource('subjects', SubjectController::class);
+Route::middleware('api')->group(function () {
+    Route::apiResource('subjects', SubjectController::class);
+});
+
+// Route::apiResource('subjects', SubjectController::class);
